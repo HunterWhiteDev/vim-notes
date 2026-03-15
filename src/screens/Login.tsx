ddf2b48d79e96c4b1a287ed486ff7b647a686893
Hunter White <hunterwhite.dev@gmail.com>
2026-03-15 15:55:37 -0400

    feat: initial commit

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 export default function Login() {
@@ -7,7 +8,7 @@ export default function Login() {
     <div className="w-sceen h-screen bg-gray-900">
       <br />
       <div className="mx-auto mt-10 w-[50%] rounded-lg border-2 border-gray-400 p-4">
-        test
+        <Input placeholder="email" />
       </div>
     </div>
   );
