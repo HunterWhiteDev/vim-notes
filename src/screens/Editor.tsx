000b86b644af16cb57ea01b797fd08a42fe2f25c
Hunter White <hunterwhite.dev@gmail.com>
2026-05-01 12:21:09 -0400

    feat: add vim toggle

--- src/screens/Editor.tsx
+++ src/screens/Editor.tsx
@@ -1,4 +1,10 @@
-import { Ref, RefObject, useEffect, useRef, useState } from "react";
+import {
+  ChangeEventHandler,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { EditorState } from "@codemirror/state";
 import {
   drawSelection,
@@ -12,6 +18,7 @@ import { vim } from "@replit/codemirror-vim";
 import darkTheme from "@/extensions/darkTheme";
 import onUpdate from "@/extensions/onUpdate";
 import { DebouncedFunc } from "lodash";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface EditorProps {
   editorRef: RefObject<EditorView | null>;
@@ -28,21 +35,25 @@ export default function Editor({
 }: EditorProps) {
   const containerRef = useRef<any>(null);
   const [hasMounted, setHasMounted] = useState(false);
+  const [useVim, setUseVim] = useState(true);
 
-  useEffect(() => {
+  const initVim = (vimEnabled: boolean) => {
+    let extensionsArr = [
+      markdownExt,
+      onUpdate({ onUpdate: handleFileDataChange }),
+      EditorView.lineWrapping,
+      gutters(),
+      lineNumbers(),
+      markdown({ base: markdownLanguage }),
+      drawSelection(),
+      darkTheme,
+    ];
+
+    if (vimEnabled) extensionsArr.push(vim());
+
     let startState = EditorState.create({
       doc: fileData,
-      extensions: [
-        markdownExt,
-        onUpdate({ onUpdate: handleFileDataChange }),
-        EditorView.lineWrapping,
-        gutters(),
-        lineNumbers(),
-        markdown({ base: markdownLanguage }),
-        drawSelection(),
-        darkTheme,
-        vim(),
-      ],
+      extensions: extensionsArr,
     });
 
     let view = new EditorView({
@@ -54,21 +65,40 @@ export default function Editor({
 
     if (!hasMounted) view.focus();
     setHasMounted(true);
+  };
 
+  useEffect(() => {
+    initVim(useVim);
     return () => {
-      view.destroy();
+      editorRef?.current?.destroy();
     };
   }, [selectedFileIdx.current]);
+
+  const handleVimModeChange = (checkedState: boolean) => {
+    setUseVim(checkedState);
+    editorRef.current?.destroy();
+    initVim(checkedState);
+  };
 
   return (
     <div className="">
       <div className="max-h-[96vh] overflow-scroll">
         <div ref={containerRef} className="cursor-text!"></div>
       </div>
-      <div className="h-2vh fixed bottom-0 flex w-full items-center border-2 border-white bg-gray-900">
-        <div className="px-2">
-          alt+escape: focus file explorer | j: move down file | k: move up file
-          | enter: select file. | n: new file | d: delete file
+      <div className="h-2vh fixed bottom-0 flex w-full items-center border border-white bg-gray-900">
+        <div className="flex px-2 text-xs">
+          <span className="flex items-center border-r pr-1">
+            <Checkbox
+              onCheckedChange={handleVimModeChange}
+              checked={useVim}
+              className="mr-1"
+            />{" "}
+            vim mode
+          </span>
+          <span className="p-1">
+            alt+escape: focus file explorer | j: move down file | k: move up
+            file | enter: select file. | n: new file | d: delete file
+          </span>
         </div>
       </div>
     </div>
